630c77dca4661bf1d57b61e87136d1bafbe86662
Christopher Small <metasoarous@gmail.com>
2015-04-06 05:41:18 -0700

    Dispatch attr display

--- src/clojure_datascience/views/resources.clj
+++ src/clojure_datascience/views/resources.clj
@@ -1,15 +1,39 @@
 (ns clojure-datascience.views.resources)
 
 
+(defn alt-resource-view
+  [{:keys [title url]}]
+  [:a {:href url} title])
+
+(defmulti attr-view
+  (fn [attr & _]
+    (if (#{:alt-resources :tags} attr)
+      attr
+      ::standard-attr-view)))
+
+(defmethod attr-view ::standard-attr-view
+  [attr data]
+  data)
+
+(defmethod attr-view :alt-resources
+  [attr alt-resources]
+  (map alt-resource-view alt-resources))
+
+(defmethod attr-view :tags
+  [attr tags]
+  (clojure.string/join ", " tags))
+
+
 (defn resource-view
-  [{:keys [url author alt-resources] :as resource}]
+ [{:keys [url author alt-resources] :as resource}]
   [:div.resource
-   [:h3
+   [:h4
     [:a {:href url} (:name resource)]]
    [:ul
-    (for [attr [:author :alt-resources]]
+    (for [attr [:author :alt-resources :tags :description]]
       (when-let [v (get resource attr)]
-        [:li (str (name attr) ": " v)]))]])
+        (into [:li (str (name attr) ": ")]
+              (attr-view attr v))))]])
 
 (defn resources-view
   [resources]
