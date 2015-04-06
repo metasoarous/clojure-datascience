9e055aba7e4eaf7f959e98bedd1584f2ec36b616
Christopher Small <metasoarous@gmail.com>
2015-04-06 05:15:26 -0700

    Generally fixed the resource view templates

--- src/clojure_datascience/views/resources.clj
+++ src/clojure_datascience/views/resources.clj
@@ -2,13 +2,13 @@
 
 
 (defn resource-view
-  [{:keys [name url author alt-resources] :as resource}]
+  [{:keys [url author alt-resources] :as resource}]
   [:div.resource
-   [:h2
-    [:a {:href url} name]]
+   [:h3
+    [:a {:href url} (:name resource)]]
    [:ul
     (for [attr [:author :alt-resources]]
-      (when-let [v (resource attr)]
+      (when-let [v (get resource attr)]
         [:li (str (name attr) ": " v)]))]])
 
 (defn resources-view
@@ -17,9 +17,9 @@
        (group-by :category)
        (map
          (fn [[cat cat-resources]]
-           [:div.category
-            [:h2 {:id (str "cat-" cat)} cat]
-            ;(map resource-view cat-resources)]))
-            ]))
+           (into
+             [:div.category
+              [:h2 {:id (str "cat-" cat)} cat]]
+             (map resource-view cat-resources))))
        (into [:div#resources])))
 
