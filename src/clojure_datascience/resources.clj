a25f9cfe0ee3804fd647df5e99e4689385525d7c
Christopher Small <metasoarous@gmail.com>
2015-04-26 11:08:41 -0700

    Adding post to resources model

--- src/clojure_datascience/resources.clj
+++ src/clojure_datascience/resources.clj
@@ -1,5 +1,6 @@
 (ns clojure-datascience.resources
   (:require [monger.core :as mg]
+            [monger.collection :as mc]
             [com.stuartsierra.component :as component]
             [taoensso.timbre :as timbre :refer [log info]]))
 
@@ -10,15 +11,36 @@
     (info "Opening mongo connection")
     (let [conn (if (:mongo-uri config)
                  (mg/connect-via-uri (:mongo-uri env))
-                 (mg/connect))]
-      (assoc component :mongo-connection conn)))
+                 (mg/connect))
+          mongo-db (mg/get-db conn "clojure-datascience")]
+      (assoc component
+             :mongo-connection conn
+             :mongo-db mongo-db)))
 
   (stop [component]
     (info "Closing mongo connection")
     (mg/disconnect (:mongo-connection component))
-    (assoc component :mongo-connection nil)))
+    (assoc component
+           :mongo-connection nil
+           :mongo-db nil)))
 
 
 (defn new-db []
   (map->MongoDatabase {}))
+
+
+(def attrs [:name :category :author :url :alt-resources :tags :description :posted-by-name :posted-by-email])
+
+(defn filter-attrs
+  [data]
+  (let [attrs-set (set attrs)]
+    (->> data
+         (filter (fn [[k v]] (attrs-set k)))
+         (into {}))))
+
+(defn post-attrs
+  [database data]
+  (mc/insert (:mongo-db database) "resources" (filter-attrs data)))
+
+
 
